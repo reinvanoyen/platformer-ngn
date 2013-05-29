94634b981572b683f7331a7e2801babda8cec1e6
Rein Van Oyen <rein@bakboord.be>
2013-05-29 22:38:08 +0200

    Improved player

--- src/Player.js
+++ src/Player.js
@@ -1,2 +1,18 @@
 var Player = ActiveObject.extend( {
+
+	constructor: function( x, y )
+	{
+		this.base();
+		this.set_texture( "sprites/bunny.png" );
+		this.set_position( x, y );
+	},
+	update: function()
+	{
+		if( Game.input_manager.is_key_down( 37 ) )
+		{
+			this.vy = 0;
+		}
+		this.move();
+	}
+	
 } );
