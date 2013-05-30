fdfdf2829a743c558002ed6ed34eed5807ac0faa
Rein Van Oyen <rein@bakboord.be>
2013-05-30 20:08:59 +0200

    refined level collision detection + added sliding

--- src/CollisionObserver.js
+++ src/CollisionObserver.js
@@ -11,26 +11,44 @@ var CollisionObserver = Base.extend( {
 	{
 		var player = Game.world.active_objects.get( 'player' );
 		var level = Game.world.level;
-		var c = player.get_position();
 		
-		var current_coord_index = Math.floor( c.x/50 );
-		
-		var a = level.level_data.coordinates[ current_coord_index ];
-		var b = level.level_data.coordinates[ current_coord_index + 1 ];
-		
-		var slope_m = ( a.y - b.y ) / ( a.x - b.x );
-		var intercept_b = a.y - slope_m * a.x;
-		var interception = slope_m * c.x + intercept_b;
-
-		// Check if point is under line
-		var position_to_line = (b.x - a.x) * (c.y - a.y) - (b.y - a.y) * (c.x - a.x);
-		
-		if( position_to_line >= 0 )
+		if( level.is_loaded )
 		{
-			player.vy = 0;
+			var c = player.get_position();
 			
-			// Set the player position to where it intersected with the line
-			player.sprite.position.y = interception;
+			var current_coord_index = Math.floor( c.x/50 );
+			
+			var a = level.level_data.coordinates[ current_coord_index ];
+			var b = level.level_data.coordinates[ current_coord_index + 1 ];
+			
+			if( a && b )
+			{
+				var slope_m = ( a.y - b.y ) / ( a.x - b.x );
+				var intercept_b = a.y - slope_m * a.x;
+				var interception = slope_m * c.x + intercept_b;
+				
+				// Check if we need to slide
+				if( slope_m > 1 || slope_m < -1 )
+				{
+					player.vx_dmg = slope_m;
+				}
+				else
+				{
+					player.vx_dmg = 0;
+				}
+		
+				// Check if point is under line
+				var position_to_line = (b.x - a.x) * (c.y - a.y) - (b.y - a.y) * (c.x - a.x);
+				
+				if( position_to_line >= 0 )
+				{
+					// Bounce the object
+					player.vy = -Math.floor( player.vy * player.bounce_factor );
+					
+					// Set the player position to where it intersected with the line
+					player.sprite.position.y = interception;
+				}
+			}
 		}
 	}
 	
