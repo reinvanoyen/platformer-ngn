b0150e6643284bc7dedeff5deffa5645e09a6860
Rein Van Oyen <rein@bakboord.be>
2013-05-30 17:36:40 +0200

    improved movement

--- src/Player.js
+++ src/Player.js
@@ -9,26 +9,35 @@ var Player = ActiveObject.extend( {
 	},
 	update: function()
 	{
-		if( this.sprite.position.y > 300 )
-		{
-			this.vy = 0;
-		}
-		
 		if( this.vy === 0 )
 		{
 			if( Game.input_manager.is_key_down( 32 ) )
 			{
-				this.vy = -5;
+				this.vy = -10;
 			}
 		}
 		
 		if( Game.input_manager.is_key_down( 37 ) )
 		{
-			this.vx = -3;
+			if( this.vy !== 0 )
+			{
+				this.vx = -3;
+			}
+			else
+			{
+				this.vx = -5;
+			}
 		}
 		else if( Game.input_manager.is_key_down( 39 ) )
 		{
-			this.vx = 3;
+			if( this.vy !== 0 )
+			{
+				this.vx = 3;
+			}
+			else
+			{
+				this.vx = 5;
+			}
 		}
 		else
 		{
