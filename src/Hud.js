cff144c681dcbf8abb158d69b28c1589359d6d02
Rein Van Oyen <rein@bakboord.be>
2013-05-30 17:36:09 +0200

    created Hud

---
+++ src/Hud.js
@@ -0,0 +1,12 @@
+var Hud = Base.extend( {
+	
+	constructor: function()
+	{
+	},
+	update: function()
+	{
+		// console.log( 'Velocity Y: ' + Game.world.active_objects.get( 'player' ).vy );
+		// console.log( 'Velocity X: ' + Game.world.active_objects.get( 'player' ).vx );
+	}
+	
+} );
