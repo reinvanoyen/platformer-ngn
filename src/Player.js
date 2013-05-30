c6d49d4a67d2c0fcd3f7be525adacc09f2384701
Rein Van Oyen <rein@bakboord.be>
2013-05-30 02:54:31 +0200

    added basic player movement (left / right / jump)

--- src/Player.js
+++ src/Player.js
@@ -5,13 +5,36 @@ var Player = ActiveObject.extend( {
 		this.base();
 		this.set_texture( "sprites/bunny.png" );
 		this.set_position( x, y );
+		this.draw( Game.stage );
 	},
 	update: function()
 	{
-		if( Game.input_manager.is_key_down( 37 ) )
+		if( this.sprite.position.y > 300 )
 		{
 			this.vy = 0;
 		}
+		
+		if( this.vy === 0 )
+		{
+			if( Game.input_manager.is_key_down( 32 ) )
+			{
+				this.vy = -5;
+			}
+		}
+		
+		if( Game.input_manager.is_key_down( 37 ) )
+		{
+			this.vx = -3;
+		}
+		else if( Game.input_manager.is_key_down( 39 ) )
+		{
+			this.vx = 3;
+		}
+		else
+		{
+			this.vx = 0;
+		}
+		
 		this.move();
 	}
 	
