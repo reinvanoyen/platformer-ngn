71c319722a50883489bac61efd5d2145a5c217f6
Rein Van Oyen <rein@bakboord.be>
2013-06-01 03:57:22 +0200

    changes to go along the new level data type

--- src/CollisionObserver.js
+++ src/CollisionObserver.js
@@ -15,39 +15,51 @@ var CollisionObserver = Base.extend( {
 		{
 			var c = this.player.get_position();
 			
-			var current_coord_index = Math.floor( c.x/50 );
+			var grid_x = Math.floor( c.x / this.level.data.grid_size );
+			var grid_y = Math.floor( c.y / this.level.data.grid_size );
 			
-			var a = this.level.level_data.coordinates[ current_coord_index ];
-			var b = this.level.level_data.coordinates[ current_coord_index + 1 ];
+			var line_data = this.level.data.collision_data[ grid_x ];
 			
-			if( a && b )
+			if( line_data )
 			{
-				var slope_m = ( a.y - b.y ) / ( a.x - b.x );
-				var intercept_b = a.y - slope_m * a.x;
-				var interception = slope_m * c.x + intercept_b;
-				
-				// Check if point is under line
-				var position_to_line = (b.x - a.x) * (c.y - a.y) - (b.y - a.y) * (c.x - a.x);
-				
-				if( position_to_line >= 0 )
+				if( line_data[ grid_y ] )
 				{
-					// Check if we need to slide
-					if( slope_m > 1 || slope_m < -1 )
-					{
-						this.player.vx_dmg = slope_m;
-					}
-					else
-					{
-						this.player.vx_dmg = 0;
-					}
-				
-					// Bounce the object
-					this.player.vy = -Math.floor( this.player.vy * this.player.bounce_factor );
+					// We gathered our line information
+					var ax = ( line_data[ grid_y ][ 0 ] + grid_x ) * this.level.data.grid_size;
+					var ay = ( line_data[ grid_y ][ 1 ] + grid_y ) * this.level.data.grid_size;
+					var bx = ( line_data[ grid_y ][ 2 ] + grid_x ) * this.level.data.grid_size;
+					var by = ( line_data[ grid_y ][ 3 ] + grid_y ) * this.level.data.grid_size;
 					
-					// Set the player position to where it intersected with the line
-					this.player.sprite.position.y = interception;
+					// Check if point is under line
+					var position_to_line = (bx - ax) * (c.y - ay) - (by - ay) * (c.x - ax);
+					
+					if( position_to_line >= 0 )
+					{
+						// Do some crazy shit
+						var slope_m = ( ay - by ) / ( ax - bx );
+						var intercept_b = ay - slope_m * ax;
+						var interception = slope_m * c.x + intercept_b;
+					
+						// Check if we need to slide
+						if( slope_m < -1 || slope_m > 1 )
+						{
+							this.player.vx_dmg = slope_m*2;
+						}
+						else
+						{
+							this.player.vx_dmg = 0;
+						}
+						
+						// Bounce the object
+						this.player.vy = -Math.floor( this.player.vy * this.player.bounce_factor );
+						
+						// Set the player position to where it intersected with the line
+						this.player.sprite.position.y = interception;
+					}
+					
 				}
 			}
+
 		}
 	}
 	
