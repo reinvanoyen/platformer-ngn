9180c8f7e5e9ea4d22dd0b857c83660d300a58a0
Rein Van Oyen <rein@bakboord.be>
2013-06-01 03:58:12 +0200

    fix

--- src/PointSprite.js
+++ src/PointSprite.js
@@ -3,7 +3,10 @@ var PointSprite = ActiveObject.extend( {
 	constructor: function( x, y )
 	{
 		this.base();
-		this.set_texture( "sprites/point.png" );
+		
+		var texture = PIXI.Texture.fromImage( "sprites/point.png" );
+		
+		this.set_texture( texture );
 		this.set_position( x, y );
 		this.draw( Game.stage );
 	},
